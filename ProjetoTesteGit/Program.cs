a75d5d4ef70229b32781e77f68751f1a92791ae1
marciogsc <marciogsc1@gmail.com>
2018-09-18 16:41:12 -0300

    Projeto iniciado mostrando Olá mundo

---
+++ ProjetoTesteGit/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ProjetoTesteGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá mundo!");
+            Console.ReadLine();
+        }
+    }
+}
