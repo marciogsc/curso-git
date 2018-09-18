0c2594c42b469565a7c995b305bc8f6c9a5679e1
marciogsc <marciogsc1@gmail.com>
2018-09-18 17:31:47 -0300

    acrescentando a mensagem - desejando um com dia

--- ProjetoTesteGit/Program.cs
+++ ProjetoTesteGit/Program.cs
@@ -7,8 +7,8 @@ namespace ProjetoTesteGit
         static void Main(string[] args)
         {
             Console.WriteLine("Olá mundo!");
-            Console.WriteLine("Boa tarde!");
             Console.WriteLine("Como você está hoje?");
+            Console.WriteLine("Tenha um bom dia!");
             Console.ReadLine();
         }
     }
